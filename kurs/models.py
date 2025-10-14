3d4df6fd691bd6dcfc8d0586fa83739aebdd7db9
pousik <nazir_magomedov06@mail.ru>
2025-10-14 21:49:13 +0300

    style

--- kurs/models.py
+++ kurs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TypeArticle(models.Model):
+    type = models.CharField(max_length=15)
+
+class Article(models.Model):
+    type = models.ForeignKey(TypeArticle, on_delete=models.CASCADE) #Тип статьи
+    title = models.CharField(max_length=40) #Наименование статьи
+    description = models.CharField(max_length=999) #Описание статьи
+    date = models.DateField() #Дата публикации
+    image = models.ImageField() #Изображение
+    source = models.CharField(max_length=50) #Ссылка на источник
